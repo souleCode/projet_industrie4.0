1987934e507ef9f4bc7eb30f82a348e118727057
malama chali <168759312+neoXmito@users.noreply.github.com>
2025-06-05 17:19:40 +0100

    Update auth_routes.py

--- server/auth_routes.py
+++ server/auth_routes.py
@@ -9,7 +9,7 @@ auth_routes = Blueprint('auth_routes', __name__)
 @auth_routes.route('/login', methods=['POST'])
 def login():
     data = request.json
-    if data.get('username') == 'admin' and data.get('password') == 'admin':
+    if data.get('username') == 'admin@admin' and data.get('password') == 'admin':
         token = create_access_token(identity=data['username'])
         return jsonify({'token': token}), 200
     else:
